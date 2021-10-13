71b2310da7adbc2f9565fcee82709ef88797199d
magic76 <shanliyoung@gmail.com>
2021-10-13 20:42:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, 'your id: ' + chatId);
 });
 
 const server = express();
